ec90a9cd8a2e204988b23104c13ea6064b556e70
Jorge Munoz <J_O_R_G_E_13@hotmail.com>
2012-07-11 22:44:55 -0500

    Added copy assets method for required files

--- project/src/org/tecunhuman/irrlicht/IrrlichtTest.java
+++ project/src/org/tecunhuman/irrlicht/IrrlichtTest.java
@@ -7,13 +7,18 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.app.Activity;
+import android.content.res.AssetManager;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.MotionEvent;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /** */
 public class IrrlichtTest extends Activity {
@@ -25,6 +30,7 @@ public class IrrlichtTest extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         try {
+        	CopyAssets();
             Utils.unpackOnSdCard(getAssets());
         } catch (IOException e) {
             Log.i("Irrlicht", "Error in unpack");
@@ -60,6 +66,39 @@ public class IrrlichtTest extends Activity {
     static {
         System.loadLibrary("irrlicht");
         System.loadLibrary("nativeirrlicht");
-    }    
+    }  
+    
+    private void CopyAssets() {
+        AssetManager assetManager = getAssets();
+        String[] files = null;
+        try {
+            files = assetManager.list("");
+        } catch (IOException e) {
+            Log.e("tag", e.getMessage());
+        }
+        for(String filename : files) {
+            InputStream in = null;
+            OutputStream out = null;
+            try {
+              in = assetManager.open(filename);
+              out = new FileOutputStream("/sdcard/Irrlicht/" + filename);
+              copyFile(in, out);
+              in.close();
+              in = null;
+              out.flush();
+              out.close();
+              out = null;
+            } catch(Exception e) {
+                Log.e("tag", e.getMessage());
+            }       
+        }
+    }
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+          out.write(buffer, 0, read);
+        }
+    }
 }
 
